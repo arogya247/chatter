ca607baec4a2020106cf67541e61ffe5b9217f98
arogya247 <arogya247@gmail.com>
2023-10-08 17:29:38 +0530

    boilerplate code removed and header added

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+			<h1>Harmoney - Chatter</h1>
+      <h4>Type something in the box below, then hit "POST"</h4>
+    </div>
+  )
+}
+
+export default Header
